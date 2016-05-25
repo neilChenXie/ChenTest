39ca28e8d1fe1c2d3ccc4ed2caba3f48d0da463c
Chen Xie <chenxie2016@outlook.com>
2016-05-25 18:16:11 +0800

    0525

--- tu/src/main/java/com/chen/MyClient.java
+++ tu/src/main/java/com/chen/MyClient.java
@@ -5,10 +5,7 @@ import org.apache.zookeeper.ZooKeeper;
 public class MyClient {
 
 	private static ZooKeeper zk;
-	private static ZookeeperConnection conn;
 	public static void main(String[] args) {
-
-	
 		
 	}
 

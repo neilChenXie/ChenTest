cb109e67ec215af20e27c664fc168075180796a3
Chen Xie <xiec@usc.edu>
2016-07-29 15:16:09 +0800

    add kafka

--- flume/simple_start/src/main/java/com/chen/SimpleInterceptor.java
+++ flume/simple_start/src/main/java/com/chen/SimpleInterceptor.java
@@ -11,16 +11,22 @@ import org.apache.flume.Event;
 import org.apache.flume.interceptor.Interceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.chen.util.SerializeUtils;
+
 /**
-* @describe
-*
-* @author Chen Xie
-* @date Jun 16, 2016
-*/
+ * @describe
+ *
+ * @author Chen Xie
+ * @date Jun 16, 2016
+ */
 public class SimpleInterceptor implements Interceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(SimpleInterceptor.class);
-	/* 
+
+	/*
 	 * @see org.apache.flume.interceptor.Interceptor#close()
 	 */
 	@Override
@@ -28,7 +34,7 @@ public class SimpleInterceptor implements Interceptor {
 
 	}
 
-	/* 
+	/*
 	 * @see org.apache.flume.interceptor.Interceptor#initialize()
 	 */
 	@Override
@@ -36,18 +42,29 @@ public class SimpleInterceptor implements Interceptor {
 
 	}
 
-	/* 
-	 * @see org.apache.flume.interceptor.Interceptor#intercept(org.apache.flume.Event)
+	/*
+	 * @see org.apache.flume.interceptor.Interceptor#intercept(org.apache.flume.
+	 * Event)
 	 */
 	@Override
 	public Event intercept(Event event) {
 		logger.info("!!!!enter intercept!!!!!");
 		Map<String, String> headers = event.getHeaders();
-		headers.put("chenxie", "hello world");
+		// String body = (String) SerializeUtils.deserialize(event.getBody());
+		// String body = new String(event.getBody());
+		// logger.info("body: {}", body);
+		// JSONObject bd = null;
+		// try {
+		// bd = JSON.parseObject(body);
+		// logger.info((String) bd.get("cookie_JSESSIONID"));
+		// } catch (Exception e) {
+		// logger.error("JSON parse error: {}", e);
+		// }
+		headers.put("User", "TBA");
 		return event;
 	}
 
-	/* 
+	/*
 	 * @see org.apache.flume.interceptor.Interceptor#intercept(java.util.List)
 	 */
 	@Override
@@ -58,9 +75,8 @@ public class SimpleInterceptor implements Interceptor {
 		}
 		return events;
 	}
-	
-	public static class Builder implements Interceptor.Builder
-	{
+
+	public static class Builder implements Interceptor.Builder {
 
 		@Override
 		public void configure(Context context) {
@@ -72,6 +88,5 @@ public class SimpleInterceptor implements Interceptor {
 			return new SimpleInterceptor();
 		}
 	}
-	
 
 }
